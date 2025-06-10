320772b3d0a64f2f5222d1da2ef1df80fe4ccb54
amar-enkhbat <enkhbat.amar@gmail.com>
2025-06-10 23:33:30 +0200

    add experiment duration calculator

--- vsync_sensor_test/vsync_sensor_frame_single_for_loop.py
+++ vsync_sensor_test/vsync_sensor_frame_single_for_loop.py
@@ -1,17 +1,15 @@
 import pyglet
-from pyglet.window import key
 import time
 from pylsl import StreamInfo, StreamOutlet
 
 # Stream setup for LSL
 info = StreamInfo(name='ScreenMarkers', type='Markers', channel_count=1,
-                  nominal_srate=0.0, channel_format='string', source_id='screen123')
+                  nominal_srate=0, channel_format='string', source_id='screen123')
 outlet = StreamOutlet(info)
 
 # Create a fullscreen window
-window = pyglet.window.Window(fullscreen=True, vsync=True)
-keys = key.KeyStateHandler()
-window.push_handlers(keys)
+config = pyglet.gl.Config(double_buffer=True)
+window = pyglet.window.Window(fullscreen=True, vsync=True, config=config)
 
 # Colors for the background
 white = (255, 255, 255)
@@ -19,23 +17,45 @@ black = (0, 0, 0)
 
 # Background rectangle that covers the whole window
 background = pyglet.shapes.Rectangle(0, 0, window.width, window.height, color=black)
-background.draw()
-window.flip()
+frame_rate = 30
+frame_duration = 1 / frame_rate
+total_duration = 5 # seconds
+total_frames = total_duration * frame_rate
 
-# Tracking state of key presses
-on_off_flag = False
-
-
-for i in range(1000):
+def on_flip(color):
+    background.color = color
     window.dispatch_events()
-    if i % 2 == 0:
-        background.color = white
-    else:
-        background.color = black
     window.clear()
     background.draw()
     window.flip()
-    if on_off_flag:
+    pyglet.gl.glFinish()
+    if color == white:
         outlet.push_sample(['1'])
-    else:
+    elif color == black:
         outlet.push_sample(['0'])
+    else:
+        raise ValueError('color not white or black')
+
+def perf_wait(dt):
+    start_time = time.perf_counter()
+    while time.perf_counter() < start_time + dt:
+        pass
+    
+
+for i in range(total_frames):
+    if i%2 == 0:
+        color = black
+    else:
+        color = white
+    
+    start_time = time.perf_counter()
+    on_flip(color)
+    duration = time.perf_counter() - start_time
+    if duration < frame_duration:
+        perf_wait(frame_duration - duration)
+    else:
+        ValueError('duration > frame duration')
+    end_time = time.perf_counter()
+
+    print(start_time, end_time, end_time - start_time)
+    
