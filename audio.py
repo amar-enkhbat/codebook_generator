87474fc5b86b11a26e66ee0a2a32db992d5a2058
amar-enkhbat <enkhbat.amar@gmail.com>
2025-08-15 12:24:53 +0200

    update audio and button box modules

--- audio.py
+++ audio.py
@@ -3,69 +3,79 @@ import numpy as np
 from psychopy import sound, tools, prefs
 from utils import perf_sleep
 from button_box import ButtonBoxController
-
+from pylsl import StreamInfo, StreamOutlet, cf_string
 
 class AudioController:
-    def __init__(self, audio_path: str, button_box: ButtonBoxController, marker_outlet):
+    def __init__(self, audio_path: str, button_box: ButtonBoxController):
         self.audio_path = audio_path
         self.button_box = button_box
-        self.marker_outlet = marker_outlet
+        self.playback_interval = 3 # Play audio every 3 seconds
+        self.key_press_pause_duration = (3, 5) # Pause between 3 to 4 seconds after key press
+        self.key_press_pause_duration = (0, 0.5) # Pause between 3 to 4 seconds after key press
+
+        # Init marker stream
+        info = StreamInfo(name='AudioCueMarkerStream', type='Marker', channel_count=1, channel_format=cf_string, nominal_srate=0, source_id='audio_marker_stream_id')
+        self.marker_outlet = StreamOutlet(info)
+
+    def random_wait(self):
+        """wait 3 to 4 seconds or values defined in self.key_press_pause_duration"""
+        val = round(np.random.uniform(self.key_press_pause_duration[0], self.key_press_pause_duration[1]), 2)
+        perf_sleep(val)
 
     def cue_audio(self, ref_obj: str, target_obj: str, mode: str):
         """Cue audio before a trial. First cue cannot be cancelled."""
         audio = sound.Sound(f'{self.audio_path}/{mode}_{ref_obj}2{target_obj}.mp3')
+        duration = audio.getDuration()
+
+        # Play first cue. It cannot be cancelled.
+        self.marker_outlet.push_sample(['play'])
         audio.play()
         start_time = time.perf_counter()
-        duration = audio.getDuration()
         perf_sleep(duration)
+        self.marker_outlet.push_sample(['stop'])
+        _ = self.button_box.read() # Flush previous key presses
         
-        # 3-second pause between playbacks, checking for input
-        end_time = time.perf_counter() + 3
+        # 3-second pause between playbacks, checking for key press
+        end_time = time.perf_counter() + self.playback_interval
         while time.perf_counter() < end_time:
-            try:
-                x = self.button_box.read()
-                if len(x):
-                    self.marker_outlet.push_sample(['button_press'])
-                    audio.stop()
-                    self.marker_outlet.push_sample(['audio_stop'])
-                    perf_sleep(np.random.randint(30, 41, 1).item() / 10)  # wait 3 to 4 seconds
-                    return x
-            except:
+            x = self.button_box.read()
+            if len(x):
+                button_box.marker_outlet.push_sample([1])
+                self.marker_outlet.push_sample(['button_press'])
+                self.random_wait()
+                return x
+            else:
                 continue
 
+        # Contiue repeating audio cue
         while True:
-            # Play the audio
+            # Play the audio until key press
             audio.play()
             start_time = time.perf_counter()
-            duration = audio.getDuration()
-
-            # Monitor button input during audio playback
+            self.marker_outlet.push_sample(['play'])
+            
             while time.perf_counter() - start_time < duration:
-                try:
-                    x = self.button_box.read()
-                    if len(x):
-                        self.marker_outlet.push_sample(['button_press'])
-                        audio.stop()
-                        perf_sleep(np.random.randint(30, 41, 1).item() / 10)  # wait 3 to 4 seconds
-                        return x
-                except:
+                x = self.button_box.read()
+                if len(x):
+                    button_box.marker_outlet.push_sample([1])
+                    audio.stop()
+                    self.marker_outlet.push_sample(['button_press'])
+                    self.random_wait()
+                    return x
+                else:
                     continue
-            # Monitor button input during audio playback
-            while time.perf_counter() - start_time < duration:
-                pass
+            self.marker_outlet.push_sample(['stop'])
 
             # 3-second pause between playbacks, still checking for input
             end_time = time.perf_counter() + 3
             while time.perf_counter() < end_time:
-                try:
-                    x = self.button_box.read()
-                    if len(x):
-                        self.marker_outlet.push_sample(['button_press'])
-                        audio.stop()
-                        self.marker_outlet.push_sample(['audio_stop'])
-                        perf_sleep(np.random.randint(30, 41, 1).item() / 10)  # wait 3 to 4 seconds
-                        return x
-                except:
+                x = self.button_box.read()
+                if len(x):
+                    button_box.marker_outlet.push_sample([1])
+                    self.marker_outlet.push_sample(['button_press'])
+                    self.random_wait()
+                    return x
+                else:
                     continue
 
     def cue_audio_single(self, ref_obj: str, target_obj: str, mode: str):
@@ -75,10 +85,7 @@ class AudioController:
         audio.play()
         start_time = time.perf_counter()
         duration = audio.getDuration() + 2
-
-        # Monitor button input during audio playback
-        while time.perf_counter() - start_time < duration:
-            pass
+        perf_sleep(duration)
         
     def get_available_audio_devices(self):
         return list(tools.systemtools.getAudioDevices().keys())
@@ -87,11 +94,7 @@ class AudioController:
         # Set speakers
         prefs.hardware['audioLib'] = ['PTB'] if ['PTB'] in prefs.hardware['audioLib'] else prefs.hardware['audioLib'] # Most time accurate library according to psychopy docs
         
-        # default_audio_devices = [
-        #     'OUT 3-4 (BEHRINGER X-AIR)', # For lab PC
-        #     'Speakers (Realtek(R) Audio)', # For Windows
-        #     'Speakers (High Definition Audio Device)' # For my mac
-        # ]
+
 
         while True:
             print("Available audio devices:\nFOR EXPERIMENT SELECT 'OUT 3-4 (BEHRINGER X-AIR)'\n")
@@ -102,6 +105,22 @@ class AudioController:
             audio_device = audio_devices[audio_device_id]
             print(audio_device)
             prefs.hardware['audioDevice'] = audio_device
-            self.cue_audio_single(self.objects[0], self.objects[1])
+            self.cue_audio_single('can', 'candle', 'scene')
             if input('Do the speakers work? y/n\n') == 'y':
                 break
+
+
+
+if __name__ == '__main__':
+    button_box = ButtonBoxController(timeout=0.1)
+    audio = AudioController('./tts/queries/psychopy_slowed', button_box=button_box)
+    # audio.select_speakers()
+    # default_audio_devices = [
+    #     'OUT 3-4 (BEHRINGER X-AIR)', # For lab PC
+    #     'Speakers (Realtek(R) Audio)', # For Windows
+    #     'Speakers (High Definition Audio Device)' # For my mac
+    # ]
+    prefs.hardware['audioDevice'] = 'OUT 3-4 (BEHRINGER X-AIR)'
+    # while True:
+    x = audio.cue_audio('can', 'candle', 'scene')
+    print(x)
--- button_box.py
+++ button_box.py
@@ -1,33 +1,42 @@
 import serial
 import logging
+from pylsl import StreamInfo, StreamOutlet
 
 
 class ButtonBoxController:
-    def __init__(self, port='COM6', baud_rate=115200, timeout=1.0):
+    def __init__(self, port='COM6', baud_rate=115200, timeout=0.01):
         self.port = port
         self.baud_rate = baud_rate
         self.timeout = timeout
-        self.button_box = None
+        self.connect()
+
+        # Init marker stream
+        info = StreamInfo(name='ButtonBoxMarkerStream', type='Marker', channel_count=1, channel_format=6, nominal_srate=0, source_id='button_box_marker_stream_id')
+        self.marker_outlet = StreamOutlet(info)
 
     def connect(self) -> None:
-        try:
-            self.button_box = serial.Serial(self.port, self.baud_rate, timeout=self.timeout)
-            self.button_box.write('A1'.encode())
-            self.button_box.reset_input_buffer()
-            self.button_box.flush()
-            text = ""
-            for _ in range(100):
-                text = text + self.button_box.read().decode()
-                if "BITSI mode, Ready!\r\n" in text:
-                    logging.info("Button box ready!")
-                    break
-        except Exception as e:
-            self.button_box = None
-            logging.warning(f"Button box not connected: {e}")
+        self.button_box = serial.Serial(self.port, self.baud_rate, timeout=self.timeout)
+        self.button_box.write('A1'.encode())
+        self.button_box.reset_input_buffer()
+        self.button_box.flush()
+        text = ""
+        for _ in range(200):
+            text = text + self.button_box.read().decode()
+            if "BITSI mode, Ready!\r\n" in text:
+                print("Button box ready!")
+                break
 
     def read(self) -> str:
-        return self.button_box.readline().decode() if self.button_box else ""
+        val = self.button_box.readline().decode()
+        return val
+
     
     def close(self) -> None:
         if self.button_box is not None:
             self.button_box.close()
+
+
+if __name__ == '__main__':
+    button_box = ButtonBoxController()
+    while True:
+        print(button_box.read())
