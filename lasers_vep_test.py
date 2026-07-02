af57aa6363d36b19881c7a01063b0c89b93989fc
amar-enkhbat <samuwarashi@gmail.com>
2026-07-02 15:10:02 +0200

    for short VEP testing recording

---
+++ lasers_vep_test.py
@@ -0,0 +1,195 @@
+import serial
+import logging
+from typing import List
+import time
+import numpy as np
+from pylsl import StreamOutlet, StreamInfo, cf_string
+from utils import perf_sleep, load_codebooks_block_2, load_codebooks_block_3, load_codebooks_block_1, random_wait
+
+class LaserController:
+    """Laser is always on except right before start of a trial"""
+    def __init__(self, port: str='COM7'):
+        self.port = port
+        self.teensy = None
+        try:
+            self.teensy = serial.Serial(port=port, baudrate=115200, timeout=1)
+        except Exception as e:
+            logging.warning(f"Teensy not connected: {e}")
+
+        # Init marker stream
+        info = StreamInfo(name='LaserMarkerStream', type='Marker', channel_count=1, channel_format=cf_string, nominal_srate=0, source_id='laser_marker_stream_id')
+        self.marker_outlet = StreamOutlet(info)
+
+        # Turn on lasers
+        self.on()
+
+    def send_lasers_values(self, values: List[int]) -> None:
+        if self.teensy is not None:
+            new_order = [0, 1, 2, 4, 5, 3, 6, 7] # For fixing laser orders being not accurate in the Teensy board
+            values = [values[i] for i in new_order]
+            values = values[::-1] # Mirror the sequences so that the stimuli are correctly presented
+            data_str = ",".join(map(str, values)) + "\n"
+            self.teensy.write(data_str.encode())  # Send data
+        else:
+            logging.warning("Teensy not connected, values not sent.")
+        
+    def on(self) -> None:
+        self.send_lasers_values([1] * 8)
+
+    def off(self) -> None:
+        self.send_lasers_values([0] * 8)
+        
+    def on_for(self, duration: float) -> None:
+        end_time = time.perf_counter() + duration
+        self.on()
+        while time.perf_counter() <= end_time:
+            pass
+        
+    def off_for(self, duration: float) -> None:
+        end_time = time.perf_counter() + duration
+        self.off()
+        while time.perf_counter() <= end_time:
+            pass
+
+    def run_trial_erp(self, codebook: List[int], target_id: int, trial_id: int, run_id: int, on_duration: float=0.1, off_duration: float=0.15):
+        """Run a single trial with multiple sequences on objects using lasers"""
+        self.marker_outlet.push_sample([f'erp;start;{trial_id};{run_id};null;{target_id};null;{[0] * 8}']) # Push trial start marker
+        for seq_id, sequence in enumerate(codebook):
+            is_target = sequence[target_id]
+            # Turn on lasers
+            end_time = time.perf_counter() + on_duration
+            self.send_lasers_values(sequence)
+            self.marker_outlet.push_sample([f'erp;null;{trial_id};{run_id};{is_target};{target_id};{seq_id};{sequence}']) # Push target marker
+            while time.perf_counter() <= end_time:
+                pass
+            # Turn off lasers
+            end_time = time.perf_counter() + off_duration
+            self.off()
+            while time.perf_counter() <= end_time:
+                pass
+
+        self.marker_outlet.push_sample([f'erp;end;{trial_id};{run_id};null;{target_id};null;{[0] * 8}']) # Push trial end marker
+
+    def run_trial_kolkhorst(self, codebook: List[int], target_id: int, trial_id: int, run_id: int, on_duration: float=0.1, off_duration: float=0.15):
+        """Run a single trial with multiple sequences on objects using lasers"""
+        self.marker_outlet.push_sample([f'kolkhorst;start;{trial_id};{run_id};null;{target_id};null;{[0] * 8}']) # Push trial start marker
+        for seq_id, sequence in enumerate(codebook):
+            is_target = sequence[target_id]
+            # Turn on lasers
+            end_time = time.perf_counter() + on_duration
+            self.send_lasers_values(sequence)
+            self.marker_outlet.push_sample([f'kolkhorst;null;{trial_id};{run_id};{is_target};{target_id};{seq_id};{sequence}']) # Push target marker
+            while time.perf_counter() <= end_time:
+                pass
+            # Turn off lasers
+            end_time = time.perf_counter() + off_duration
+            self.off()
+            while time.perf_counter() <= end_time:
+                pass
+
+        self.marker_outlet.push_sample([f'kolkhorst;end;{trial_id};{run_id};null;{target_id};null;{[0] * 8}']) # Push trial end marker
+
+    
+    def run_trial_cvep(self, codebook: List[int], target_id: int, trial_id: int, run_id: int, on_duration: float=1 / 60):
+        """Run a single trial with multiple sequences on objects using lasers"""
+        self.marker_outlet.push_sample([f'cvep;start;{trial_id};{run_id};null;{target_id};null;{[0] * 8}']) # Push trial start marker
+        for seq_id, sequence in enumerate(codebook):
+            is_target = sequence[target_id]
+            # Turn on lasers
+            end_time = time.perf_counter() + on_duration
+            self.send_lasers_values(sequence)
+            self.marker_outlet.push_sample([f'cvep;null;{trial_id};{run_id};{is_target};{target_id};{seq_id};{sequence}']) # Push target marker
+            while time.perf_counter() <= end_time:
+                pass
+
+        self.marker_outlet.push_sample([f'cvep;end;{trial_id};{run_id};null;{target_id};null']) # Push trial end marker
+
+        # Turn off everything after end of the trial
+        self.off()
+    
+    def test_erp(self, n_trials=8):
+        """Test Run ERP protocol"""
+        codebook = load_codebooks_block_2()[0].astype(int).tolist()
+        # codebook = load_codebooks_block_1()[0].astype(int).tolist()
+
+        trial_run_times = []
+        for trial_id in range(n_trials):
+            start_time = time.perf_counter()
+            self.run_trial_erp(codebook, target_id=0, trial_id=trial_id, run_id=999, on_duration=0.1, off_duration=0.15)
+            elapsed_time = time.perf_counter() - start_time
+            trial_run_times.append(elapsed_time)
+            perf_sleep(3)
+
+        print('Trial run times:', trial_run_times)
+        print('Mean trial run time (should be 12):', np.mean(trial_run_times))
+
+    def test_erp_kolkhorst(self, n_trials=8):
+        """Test Run ERP protocol"""
+        codebook = load_codebooks_block_1()[0].astype(int).tolist()
+
+        trial_run_times = []
+        for trial_id in range(n_trials):
+            start_time = time.perf_counter()
+            print(len(codebook))
+            self.run_trial_kolkhorst(codebook, target_id=0, trial_id=trial_id, run_id=999, on_duration=0.1, off_duration=0.15)
+            elapsed_time = time.perf_counter() - start_time
+            trial_run_times.append(elapsed_time)
+            perf_sleep(3)
+
+        print('Trial run times:', trial_run_times)
+        print('Mean trial run time (should be 12):', np.mean(trial_run_times))
+
+
+    def test_cvep(self, n_trials=8):
+        """Test Run ERP protocol"""
+        codebook = load_codebooks_block_3()[0].astype(int).tolist()
+        codebook = codebook[:720]
+
+        trial_run_times = []
+        for trial_id in range(n_trials):
+            start_time = time.perf_counter()
+            self.run_trial_cvep(codebook, target_id=0, trial_id=trial_id, run_id=999, on_duration=2)
+            elapsed_time = time.perf_counter() - start_time
+            trial_run_times.append(elapsed_time)
+            perf_sleep(3)
+
+        print('Trial run times:', trial_run_times)
+        print('Mean trial run time (should be 12):', np.mean(trial_run_times))
+
+    def close(self) -> None:
+        if self.teensy is not None:
+            self.off()
+            self.teensy.close()
+
+    def test_laser_order(self) -> None:
+        for i in range(8):
+            seq = [0] * 8
+            seq[i] = 1
+            self.send_lasers_values(seq)
+            perf_sleep(1)
+
+if __name__ == '__main__':
+    lasers = LaserController(port="COM14")
+    # lasers.on()
+    lasers.off()
+    _ = input('Press any key to continue.\n')
+    # lasers.off()
+    # lasers.test_laser_order()
+    # lasers.run_quick_flash(n_trials=8)
+    # lasers.run_quick_flash(n_trials=16, wait_low=1/60, wait_high=1/60)
+    # lasers.run_isolated_flash(1)
+    # lasers.run_burst_flash(2)
+    # _ = input('Press any key!\n')
+    
+    # lasers.test_erp(1)
+    # perf_sleep(3)
+    perf_sleep(3)
+    lasers.test_cvep(1)
+
+    _ = input('Press any key to continue.\n')
+    perf_sleep(3)
+    # lasers.test_erp_kolkhorst(1)
+    # _ = input('Press any key!\n')
+    # lasers.test_erp_kolkhorst(10)
+    # _ = input('Press any key!\n')
+    
---
+++ window_vep_test.py
@@ -0,0 +1,353 @@
+import time
+import numpy as np
+from psychopy import visual, event
+from typing import Dict, List
+from pylsl import StreamInfo, StreamOutlet, cf_string
+from utils import load_codebooks_block_2, load_codebooks_block_3, random_wait
+
+
+class ScreenStimWindow:
+    def __init__(self, objects: Dict[int, str]):
+        self.objects = objects
+        self.quick_flash_wait_duration = (0.75, 1) # 0.75 to 1 seconds
+        
+
+        self.n_objs = len(objects)
+        self.screen_warmup_duration = 1 # seconds
+        self.refresh_rate = 60 # Hz
+        
+        # Screen (condition 4)
+        # self.win = visual.Window(size=(1920, 1080), winType='pyglet', fullscr=True, screen=1, units="pix", color='grey', waitBlanking=True, allowGUI=False)
+        self.win = visual.Window(size=(2560, 1440), winType='pyglet', fullscr=True, screen=1, units="pix", color='grey', waitBlanking=True, allowGUI=False)
+        self.width, self.height = self.win.size
+        self.actual_refresh_rate = int(round(self.win.getActualFrameRate(nMaxFrames=300, nWarmUpFrames=60)))
+        print(f'Calculated screen refresh rate (should be {self.refresh_rate}):', self.actual_refresh_rate)
+
+        
+        self.sensor_box_size = 80
+        self.stim_box_size = 150
+        self.stim_box_space = (self.width - self.stim_box_size * 8) // 9
+        self.stim_box_poss = [(self.stim_box_space + i*(self.stim_box_size + self.stim_box_space) - self.width // 2 + self.stim_box_size / 2, 0) for i in range(self.n_objs)]
+        self.pictogram_poss = [(self.stim_box_space + i*(self.stim_box_size + self.stim_box_space) - self.width // 2 + self.stim_box_size / 2, 0) for i in range(self.n_objs)] # Psychopy decided that positions are determined as the center of the objects
+        # Init stimulus window
+        self.init_sensor()
+        self.init_boxes(n_boxes=self.n_objs)
+        self.init_pictograms()
+        # Init description text on screen
+        self.init_text()
+
+        # Init marker stream
+        info = StreamInfo(name='ScreenMarkerStream', type='Marker', channel_count=1, channel_format=cf_string, nominal_srate=0, source_id='screen_marker_stream_id')
+        self.marker_outlet = StreamOutlet(info)
+
+    def init_sensor(self):
+        # Create a box on top left of screen for vsync sensor
+        self.sensor_box = visual.Rect(self.win, width=self.sensor_box_size, height=self.sensor_box_size, pos=(-self.width / 2 + self.sensor_box_size / 2, self.height / 2 - self.sensor_box_size / 2), color='black', autoLog=False)
+        self.sensor_box.setAutoDraw(False)
+        
+    def init_boxes(self, n_boxes: int):
+        # Boxes behind pictograms
+        if n_boxes == 1:
+            self.stim_box_poss = [(0, 0)]
+        else:
+            self.stim_box_poss = [(self.stim_box_space + i*(self.stim_box_size + self.stim_box_space) - self.width // 2 + self.stim_box_size / 2, 0) for i in range(self.n_objs)]
+        self.boxes = []
+        for i in range(n_boxes):
+            box = visual.Rect(self.win, width=self.stim_box_size, height=self.stim_box_size, pos=self.stim_box_poss[i], units='pix', color='black', autoLog=False)
+            box.setAutoDraw(False)
+            self.boxes.append(box)
+
+    def init_pictograms(self):
+        # Pictograms on top of boxes
+        self.pictograms = []
+        for i, img_path in enumerate(self.objects.values()):
+            pictogram = visual.ImageStim(self.win, f'./icons/{img_path}.png', mask=None, units='pix', pos=self.pictogram_poss[i], size=self.stim_box_size, autoLog=False)
+            pictogram.setAutoDraw(False)
+            self.pictograms.append(pictogram)
+
+    def init_text(self):
+        self.description_text = visual.TextStim(self.win, text="Hello World", pos=(0, 0), color='white', height=50, autoLog=False)
+        self.description_text.setAutoDraw(False)
+
+    def draw_text(self, text: str):
+        self.description_text.setText(text)
+        self.description_text.draw()
+        
+    def draw_boxes(self, sequence: List[int]):
+        # for val, box in zip(sequence, self.boxes):
+        for i in range(len(self.boxes)):
+            if sequence[i] == 1:
+                self.boxes[i].fillColor = 'white'
+            elif sequence[i] == 0:
+                self.boxes[i].fillColor = 'black'
+            else:
+                self.boxes[i].fillColor = 'grey'
+            self.boxes[i].draw()
+            
+    def draw_pictograms(self):
+        for pictogram in self.pictograms:
+            pictogram.draw()
+
+    def draw_sensor_box(self, color: str):
+        self.sensor_box.fillColor = color
+        self.sensor_box.draw()
+    
+    def disable_stims(self):
+        self.draw_sensor_box('grey')
+        self.draw_boxes([2] * 8)
+        self.win.flip()
+    
+    def hide_stims(self, hide_duration: float=1.0):
+        for _ in range(int(hide_duration * self.refresh_rate)):  # 1 second of flips at 60Hz
+            self.disable_stims()
+            
+    def screen_warmup(self, duration: float=1.0, draw_pictograms=True):
+        """Flip screen at refresh rate for dt seconds to warmup.
+
+        Args:
+            dt (float): warmup duration (seconds)
+        """
+        for _ in range(int(duration * self.refresh_rate)):  # 1 second of flips at 60Hz
+            self.draw_sensor_box('black')
+            self.draw_boxes([0] * 8)
+            if draw_pictograms:
+                self.draw_pictograms()
+            self.win.flip()
+            
+    def reorder_pictograms(self, new_idc: List[int]):
+        # self.pictogram_poss = [self.pictogram_poss[i] for i in new_idc]  # NOTE: Could be indexing twice.
+        # self.init_pictograms()
+        new_idc = [new_idc.index(i) for i in range(len(new_idc))]
+        new_poss = [self.pictogram_poss[i] for i in new_idc]
+        for i in range(len(new_poss)):
+            self.pictograms[i].pos = new_poss[i]
+    
+    def default_order_pictograms(self):
+        new_poss = [(self.stim_box_space + i*(self.stim_box_size + self.stim_box_space) - self.width // 2 + self.stim_box_size / 2, 0) for i in range(self.n_objs)]
+        for i in range(len(new_poss)):
+            self.pictograms[i].pos = new_poss[i]
+
+    def run_trial_erp(self, codebook: list, target_id: int, trial_id: int, run_id: int, n_stim_on_frames: int, n_stim_off_frames: int):
+        """Run a single trial with multiple sequences on monitor"""
+        
+        self.marker_outlet.push_sample([f'erp;start;{trial_id};{run_id};null;{target_id};null;{[0] * 8}']) # Push trial start marker
+        for seq_id, sequence in enumerate(codebook):
+            is_target = sequence[target_id]
+            for i in range(n_stim_on_frames):
+                self.draw_sensor_box('white' if is_target == 1 else 'black')
+                self.draw_boxes(sequence)
+                self.draw_pictograms()
+                self.win.flip()
+                # If first sequence and is_target is equal to 1 push to target outlet
+                if i == 0:
+                    self.marker_outlet.push_sample([f'erp;null;{trial_id};{run_id};{is_target};{target_id};{seq_id};{sequence}']) # Push target marker
+            for i in range(n_stim_off_frames):
+                self.draw_sensor_box('black')
+                self.draw_boxes([0] * 8)
+                self.draw_pictograms()
+                self.win.flip()
+        self.marker_outlet.push_sample([f'erp;end;{trial_id};{run_id};null;{target_id};null;{[0] * 8}']) # Push trial end marker
+
+    def run_trial_cvep(self, codebook: list, target_id: int, trial_id: int, run_id: int):
+        """Run a single trial with multiple sequences on monitor"""
+        self.marker_outlet.push_sample([f'cvep;start;{trial_id};{run_id};null;{target_id};null;{[0] * 8}']) # Push trial start marker
+        for seq_id, sequence in enumerate(codebook):
+            is_target = sequence[target_id]
+            self.draw_sensor_box('white' if is_target == 1 else 'black')
+            self.draw_boxes(sequence)
+            self.draw_pictograms()
+            self.win.flip()
+            # If first sequence and is_target is equal to 1 push to target outlet
+            self.marker_outlet.push_sample([f'cvep;null;{trial_id};{run_id};{is_target};{target_id};{seq_id};{sequence}']) # Push target marker
+                    
+        self.marker_outlet.push_sample([f'cvep;end;{trial_id};{run_id};null;{target_id};null']) # Push trial end marker
+
+        # Rest stims after trial
+        self.draw_sensor_box('black')
+        self.draw_boxes([0] * 8)
+        self.draw_pictograms()
+        self.win.flip()
+
+    def test_erp(self, n_trials: int=8):
+        """Test Run ERP protocol"""        
+        # Load codebook
+        codebooks = load_codebooks_block_2().astype(int).tolist()
+        
+        n_on_frames = 6 # 0.1 seconds
+        n_off_frames = 9 # 0.15 seconds
+        trial_run_times = []
+        self.screen_warmup(duration=3)
+        self.win.recordFrameIntervals = True
+        for trial_id in range(n_trials):
+            target_id = np.random.randint(0, self.n_objs) # Get a random target
+            start_time = time.perf_counter()
+            self.run_trial_erp(codebooks[target_id], target_id=target_id, trial_id=trial_id, run_id=999, n_stim_on_frames=n_on_frames, n_stim_off_frames=n_off_frames)
+            elapsed_time = time.perf_counter() - start_time
+            trial_run_times.append(elapsed_time)
+            self.screen_warmup(duration=3)
+
+        # Log results
+        frame_intervals = np.array(self.win.frameIntervals)
+        n_dropped_frames = sum(frame_intervals > 1.5 * (1/self.refresh_rate))
+        print(f"Avg frame interval: {frame_intervals.mean()}")
+        print(f"Min frame interval: {frame_intervals.min()}")
+        print(f"Max frame interval: {frame_intervals.max()}")
+        print(f'5 highest frame interval frame #:', np.argsort(frame_intervals)[-5:])
+        print(f"Dropped frames: {n_dropped_frames} out of {len(frame_intervals)}")
+        print(f'Specified refresh rate: {self.refresh_rate}')
+        print(f'# of dropped frames: {n_dropped_frames}')
+        self.win.recordFrameIntervals = False
+
+        print('Trial run times:', trial_run_times)
+        print('Mean trial run time (should be 12):', np.mean(trial_run_times))
+
+    def test_vep(self, n_trials: int=8):
+        """Test Run ERP protocol"""        
+        # Create new codebook with only one target
+        sequence_length = 600 # roughly 60 seconds
+        print(sequence_length)
+        sequence = []
+        while len(sequence) < sequence_length:
+            run_length = np.random.randint(5, 11)
+            zero_run = np.zeros(run_length, dtype=np.uint8).tolist()
+            sequence.extend(zero_run)
+            sequence.append(1)
+        sequence = np.array(sequence)[:sequence_length]
+
+        codebooks = np.zeros((8, sequence_length, 8), dtype=np.int8)
+        codebooks[0, :, 0] = sequence
+        print(codebooks.shape)
+        codebooks = codebooks.tolist()
+        target_id = 0
+        
+        n_on_frames = 1 # 0.01666 seconds
+        n_off_frames = 6 # 0.1 seconds
+
+        print('duration:', sequence.sum() * n_on_frames/60 - (sequence - 1).sum() * n_off_frames/60)
+        trial_run_times = []
+        self.screen_warmup(duration=3)
+        self.win.recordFrameIntervals = True
+        for trial_id in range(n_trials):
+            start_time = time.perf_counter()
+            self.run_trial_erp(codebooks[target_id], target_id=target_id, trial_id=trial_id, run_id=999, n_stim_on_frames=n_on_frames, n_stim_off_frames=n_off_frames)
+            elapsed_time = time.perf_counter() - start_time
+            trial_run_times.append(elapsed_time)
+            self.screen_warmup(duration=3)
+
+        # Log results
+        frame_intervals = np.array(self.win.frameIntervals)
+        n_dropped_frames = sum(frame_intervals > 1.5 * (1/self.refresh_rate))
+        print(f"Avg frame interval: {frame_intervals.mean()}")
+        print(f"Min frame interval: {frame_intervals.min()}")
+        print(f"Max frame interval: {frame_intervals.max()}")
+        print(f'5 highest frame interval frame #:', np.argsort(frame_intervals)[-5:])
+        print(f"Dropped frames: {n_dropped_frames} out of {len(frame_intervals)}")
+        print(f'Specified refresh rate: {self.refresh_rate}')
+        print(f'# of dropped frames: {n_dropped_frames}')
+        self.win.recordFrameIntervals = False
+
+        print('Trial run times:', trial_run_times)
+        print('Mean trial run time (should be 12):', np.mean(trial_run_times))
+    
+    def test_cvep(self, n_trials: int=8):
+        """Test Run CVEP protocol"""
+        self.screen_warmup(duration=3)
+        self.win.recordFrameIntervals = True
+        # run 10 trials with 1 warmup in-between
+        codebook = load_codebooks_block_3()[0].tolist()
+        codebook = codebook[:720]
+
+        trial_run_times = []
+        for trial_id in range(n_trials):
+            start_time = time.perf_counter()
+            self.run_trial_cvep(codebook, target_id=0, trial_id=trial_id, run_id=999)
+            elapsed_time = time.perf_counter() - start_time
+            trial_run_times.append(elapsed_time)
+            self.screen_warmup(duration=3)
+
+        # Log results
+        frame_intervals = np.array(self.win.frameIntervals)
+        n_dropped_frames = sum(frame_intervals > 1.5 * (1/self.refresh_rate))
+        print(f"Avg frame interval: {frame_intervals.mean()}")
+        print(f"Min frame interval: {frame_intervals.min()}")
+        print(f"Max frame interval: {frame_intervals.max()}")
+        print(f'5 highest frame interval frame #:', np.argsort(frame_intervals)[-5:])
+        print(f"Dropped frames: {n_dropped_frames} out of {len(frame_intervals)}")
+        print(f'Specified refresh rate: {self.refresh_rate}')
+        print(f'# of dropped frames: {n_dropped_frames}')
+        self.win.recordFrameIntervals = False
+
+        print('Trial run times:', trial_run_times)
+        print('Mean trial run time (should be 12.016):', np.mean(trial_run_times))
+
+    def test_cvep_vep(self, n_trials: int=8):
+        """Test Run CVEP protocol"""
+        self.screen_warmup(duration=3)
+        self.win.recordFrameIntervals = True
+        # run 10 trials with 1 warmup in-between
+        codebook = load_codebooks_block_3()[0]
+        print(codebook.shape)
+        codebook[:, 1:] = 0
+
+        trial_run_times = []
+        for trial_id in range(n_trials):
+            start_time = time.perf_counter()
+            self.run_trial_cvep(codebook, target_id=0, trial_id=trial_id, run_id=999)
+            elapsed_time = time.perf_counter() - start_time
+            trial_run_times.append(elapsed_time)
+            self.screen_warmup(duration=3)
+
+        # Log results
+        frame_intervals = np.array(self.win.frameIntervals)
+        n_dropped_frames = sum(frame_intervals > 1.5 * (1/self.refresh_rate))
+        print(f"Avg frame interval: {frame_intervals.mean()}")
+        print(f"Min frame interval: {frame_intervals.min()}")
+        print(f"Max frame interval: {frame_intervals.max()}")
+        print(f'5 highest frame interval frame #:', np.argsort(frame_intervals)[-5:])
+        print(f"Dropped frames: {n_dropped_frames} out of {len(frame_intervals)}")
+        print(f'Specified refresh rate: {self.refresh_rate}')
+        print(f'# of dropped frames: {n_dropped_frames}')
+        self.win.recordFrameIntervals = False
+
+        print('Trial run times:', trial_run_times)
+        print('Mean trial run time (should be 12.016):', np.mean(trial_run_times))
+
+
+def main():
+    objects = {
+        0: 'can', 
+        1: 'bandage', 
+        2: 'remote', 
+        3: 'bottle', 
+        4: 'candle', 
+        5: 'box', 
+        6: 'book', 
+        7: 'cup'
+    }
+    screen = ScreenStimWindow(objects)
+    # Set sensor box to black
+    screen.screen_warmup(3)
+    screen.draw_sensor_box('black')
+    screen.draw_text('Press any key to continue\nVEP')
+    screen.init_boxes(1)
+    screen.win.flip()
+    event.waitKeys()
+    for _ in range(5):
+        screen.screen_warmup(3)
+        screen.test_vep(1)
+        screen.screen_warmup(10)
+
+    screen.screen_warmup(3)
+    screen.draw_sensor_box('black')
+    screen.draw_text('Press any key to continue\ncVEP')
+    screen.init_boxes(1)
+    screen.win.flip()
+    event.waitKeys()
+    for _ in range(10):
+        screen.screen_warmup(3)
+        screen.test_cvep_vep(1)
+
+
+
+if __name__ == '__main__':
+    main()
